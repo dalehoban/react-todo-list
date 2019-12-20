1875c1fc71389a646dfcd40d9ec83f459ae6188b
unknown <dalehoban@gmail.com>
2019-12-19 19:57:15 -0800

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import uuid from 'uuid';
+import  'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default class TodoItem extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello from Todo Item</h3>
+      </div>
+    );
+  }
+}
